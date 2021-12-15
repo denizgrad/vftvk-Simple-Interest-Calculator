f2f435cd27b332c76c81feecfdaaf4a227f604ed
denizgrad <denizgrad@hotmail.com>
2021-12-15 22:59:21 +0100

    working final

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # coding-project-template
+
+midsection i ortada durdur
+resulttaki date i dynamic  yap
+inputu table gibi yap
+slider i degistir, kendini yenileyen ve yuzde isareti
--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
+var principal = document.getElementById("principal").value;
+alert(principal);
+var rate = document.getElementById("rate").value;
+var rateout = document.getElementById("rateoutput").value;
+var years = document.getElementById("years").value;
+var interest = (principal * years * rate) /100;
+var actualyear = (new Date().getFullYear()) + years;
+var spanresult = document.getElementById("result");
+function slider(rateval){
+    rate = rateval;
+    rateout = rate;
+    document.getElementById("rateoutput").innerText = rateout;
+    
+
+}
+function calculate()
 {
-    p = document.getElementById("principal").value;
+    amount = document.getElementById("amount").value;
+    rate = document.getElementById("rateoutput").innerText;
+    years = document.getElementById("years").value;
+    interest = (Number(amount) * Number(years) * Number(rate)) /100;
+    actualyear = (new Date().getFullYear()) + Number(years);
+    spanresult = document.getElementById("result");
+    var text = ' <br> If you deposit <mark>'+amount+'</mark>, <br>'+
+               'at an interest rate of <mark>'+rate+'%</mark>. <br>'+
+               'You will receive an amount of <mark>'+interest+'</mark>, <br>'+
+               'in the year <mark>'+actualyear.toString()+'</mark>'
+    spanresult.innerHTML = text
+    
     
 }
-        
